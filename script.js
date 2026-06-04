325ec94a53a2f1b9fd6ded9c74fed09b7d39bd0e
Austriana <Marco.Mang@gmx.de>
2026-06-04 21:56:52 +0200

    .

--- script.js
+++ script.js
@@ -2,13 +2,17 @@ let navContainer = document.getElementById("navContainer");
 let hamburgerContainer = document.getElementById("hamburgerContainer");
 
 hamburgerContainer.addEventListener("click", () => {
-    if(navContainer.style.display != "flex")
+    if(navContainer.style.display != "flex"){
         navContainer.style.display = "flex";
-    else{
-        navContainer.style.display = "none";}
+        hamburgerContainer.style.display = "none";
+    } else{
+        navContainer.style.display = "none";
+    }
 });
 
 navContainer.addEventListener("click", (e) => {
-    if(e.target.classList.value === "navlink")
+    if(e.target.classList.value === "navlink"){
         navContainer.style.display = "none";
+        hamburgerContainer.style.display = "block";
+    }
 });
