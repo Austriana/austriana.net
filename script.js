c3fa5e6046faa8352357e85984c7655bc8c8f5c6
Austriana <Marco.Mang@gmx.de>
2026-06-05 11:52:29 +0200

    hamburger adds

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 let navContainer = document.getElementById("navContainer");
 let hamburgerContainer = document.getElementById("hamburgerContainer");
+let hamburger = document.getElementById("hamburger");
 
 hamburgerContainer.addEventListener("click", () => {
     if(navContainer.style.display != "flex"){
         navContainer.style.display = "flex";
-        hamburgerContainer.style.display = "none";
+        hamburger.classList.toggle("active");
     } else{
         navContainer.style.display = "none";
+        hamburger.classList.remove("active");
     }
 });
 
 navContainer.addEventListener("click", (e) => {
     if(e.target.classList.value === "navlink"){
         navContainer.style.display = "none";
-        hamburgerContainer.style.display = "block";
+        hamburger.classList.remove("active");
     }
 });
